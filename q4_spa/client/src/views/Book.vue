b34cc5de2f9c41926f0d83d92ecb7429cde3d7cc
Sandoche Adittane <sandoche@protonmail.com>
2019-09-06 14:57:10 +0200

    :lipstick: Style implemented to all the components

--- q4_spa/client/src/views/Book.vue
+++ q4_spa/client/src/views/Book.vue
@@ -1,9 +1,11 @@
 <template>
-  <main class="book container">
-    <nav>
-      <router-link :to="{ name: 'books'}">← Back</router-link>
+  <main class="book">
+    <nav class="book__nav">
+      <div class="container">
+        <router-link :to="{ name: 'books'}">← Back</router-link>
+      </div>
     </nav>
-    <header>
+    <section class="book__section container">
       <img :src="book.cover" :alt="`${book.title} ${$t('books.cover_alt')}`" class="book__image"/>
       <div>
         <h1 class="book__title">{{ book.title }}</h1>
@@ -15,12 +17,12 @@
         :upvotes="book.upvotes"
       >
       </base-book-review>
-    </header>
-    <section>
-      <h2>{{ $t('book.synopsis') }}</h2>
-      <p>
-        {{ book.synopsis }}
-      </p>
+      <div class="book__synopsis">
+        <h2>{{ $t('book.synopsis') }}</h2>
+        <p>
+          {{ book.synopsis }}
+        </p>
+      </div>
     </section>
   </main>
 </template>
@@ -57,4 +59,43 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/variables.scss';
 @import '../styles/shared.scss';
+
+.book {
+  &__nav {
+    height: 30vh;
+    background: $primary-color;
+    padding-top: 32px;
+
+    a {
+      color: #fff;
+      font-size: $font-md;
+      font-weight: 700;
+      transition: all 0.4s ease;
+      display: block;
+
+      &:hover {
+        transform: translateX(-10px) translateY(0) translateZ(0);
+      }
+    }
+  }
+
+  &__section {
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr;
+    grid-gap: 32px;
+    margin-top: -15vh;
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+
+  &__image {
+    width: 100%;
+    box-shadow: 0 4px 12px rgba(32,43,54,0.3);
+  }
+
+  &__synopsis {
+    grid-column-start: 2;
+    grid-column-end: 4;
+  }
+}
 </style>
