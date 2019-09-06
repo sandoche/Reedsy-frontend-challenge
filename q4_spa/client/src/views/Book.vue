2d931544cff4cd264e7fb79f9273adf22de96bdd
Sandoche Adittane <sandoche@protonmail.com>
2019-09-06 11:28:20 +0200

    :construction: Page Book connected to the store

--- q4_spa/client/src/views/Book.vue
+++ q4_spa/client/src/views/Book.vue
@@ -1,5 +1,34 @@
 <template>
   <main>
-    Book page
+    {{ book }}
   </main>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Book',
+  created () {
+    this.reset()
+    this.retrieve(this.$route.params.slug)
+  },
+  methods: {
+    ...mapActions({
+      retrieve: 'book/retrieve',
+      reset: 'book/reset'
+    })
+  },
+  computed: {
+    ...mapGetters({
+      book: 'book/book',
+      error: 'book/error',
+      loading: 'book/loading'
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
